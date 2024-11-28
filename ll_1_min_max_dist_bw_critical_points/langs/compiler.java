e04ecb7e674e94cd424caa0d7cf3cd0dfbea83e2
vikastanwarblitz <150047488+vikastanwarblitz@users.noreply.github.com>
2024-11-28 23:38:22 +0530

    updated compiler and user java files

--- ll_1_min_max_dist_bw_critical_points/langs/compiler.java
+++ ll_1_min_max_dist_bw_critical_points/langs/compiler.java
@@ -1,11 +1,42 @@
-//compiler boilderplate
-class abc{
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Driver {
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
-        String myString = scanner.next();
+        String myString = scanner.nextLine();  // Read the entire line of input
         scanner.close();
 
-        Solution sol = new Solution();
-        System.out.print(Character.toString(sol.firstChar(myString)));
+        String[] values = myString.split(" "); // Split the input into individual values
+        Node head = createLinkedList(values);  // Create linked list from values
+
+        // Printing the linked list to verify
+        int[] output = Solution.solve(head);
+
+        System.out.println(Arrays.toString(output));
+
     }
+
+    // Method to create a linked list from a string array of numbers
+    public static Node createLinkedList(String[] values) {
+        if (values.length == 0) return null;  // Handle empty input
+
+        Node head = new Node();               // Initialize head node
+        head.val = Integer.parseInt(values[0]);  // Assign the first value
+        Node current = head;
+
+        for (int i = 1; i < values.length; i++) {
+            Node newNode = new Node();
+            newNode.val = Integer.parseInt(values[i]);
+            current.next = newNode;  // Link current node to the new node
+            current = newNode;       // Move to the new node
+        }
+        return head;  // Return the head of the list
+    }
+}
+
+// Node class definition
+class Node {
+    int val;
+    Node next;
 }
--- ll_1_min_max_dist_bw_critical_points/langs/user.java
+++ ll_1_min_max_dist_bw_critical_points/langs/user.java
@@ -1,11 +1,12 @@
 //User Boilerplate
 import java.util.Scanner;
 
-// Node class definition
-class Node {
+// Node class definition looks like this
+
+/*class Node {
     int val; //value stored in the current element
     Node next; //pointer to next node
-}
+}*/
 
 class Solution {
     public int[] solve(Node head) {
